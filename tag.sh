a3c0e33eede37c48e8e58bc19932d0ede6eb26aa
Ali-Akber Saifee <ali@indydevs.org>
2013-09-04 06:20:24 +0800

    fixed tagging script to commit HISTORY.rst as well

--- tag.sh
+++ tag.sh
@@ -2,8 +2,6 @@
 echo current version:$(python -c "import holmium.version;print holmium.version.__version__")
 read -p "new version:" new_version
 sed -i -e "s/__version__.*/__version__=\"${new_version}\"/g" holmium/version.py 
-git add holmium/version.py
-git commit -m "updating version to ${new_version}"
 last_portion=$(grep -E "^History$" HISTORY.rst -5 | grep -E "^\d+.\d+.\d+")
 changelog_file=/var/tmp/holmium.core.newchangelog
 new_changelog_heading="${new_version} `date +"%Y-%m-%d"`"
@@ -17,6 +15,8 @@ if rst2html.py HISTORY.rst > /dev/null
 then
     echo "tagging $new_version"
     git add HISTORY.rst 
+    git add holmium/version.py
+    git commit -m "updating version to ${new_version}"
     git tag -s $(python setup.py --version) -m "tagging version ${new_version}"
     python setup.py build sdist bdist_egg upload
 else
