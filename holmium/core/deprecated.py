37fee9bcd70f1870e925a5406c8b2e758adcbfcb
Ali-Akber Saifee <ali@indydevs.org>
2013-09-08 12:20:31 +0800

    added missing file

---
+++ holmium/core/deprecated.py
@@ -0,0 +1,23 @@
+import types
+import pageobject
+import testcase
+import warnings
+class Deprecated(object):
+    def __new__(cls, *args, **kwargs):
+        warnings.warn("The use of %s is deprecated. Use %s instead" % ( cls.cur, cls.alt.__name__)
+                , DeprecationWarning
+                , stacklevel=2 )
+        return super(Deprecated, cls).__new__(cls)
+
+def construct_deprecated(name, alt):
+    doc  = """Deprecated alias for :class:`%s`""" % alt.__name__
+    cls = types.ClassType(name, (Deprecated, alt), dict(cur=name,alt=alt, __doc__ = doc))
+    return cls
+
+PageObject = construct_deprecated("PageObject", pageobject.Page)
+PageElement = construct_deprecated("PageElement", pageobject.Element)
+PageElements = construct_deprecated("PageElements", pageobject.Elements)
+PageElementMap = construct_deprecated("PageElementMap", pageobject.ElementMap)
+HolmiumTestCase = construct_deprecated("HolmiumTestCase", testcase.TestCase)
+
+warnings.simplefilter("always")
