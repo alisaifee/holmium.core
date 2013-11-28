352c4c266ec03d9b3f755d545e3c00121db745d8
Ali-Akber Saifee <ali@indydevs.org>
2013-11-28 11:15:23 +0800

    lol..

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,5 +5,5 @@
 def test_version():
     import holmium.version
     import re
-    assert re.compile("(\d+).(\d+).(\d+)").match(holmium.version.__version__)
+    assert re.compile("(\d+).(\d+)(.\d+)?").match(holmium.version.__version__)
 
