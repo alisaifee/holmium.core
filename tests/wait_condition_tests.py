30be41b25c6ada3963f912e7de80351dff417af8
Ali-Akber Saifee <ali@indydevs.org>
2014-02-10 17:45:37 +0800

    attempt to make tests less fragile
    the conditions tests fail randomly on travis, and my sandbox vm for
    12.04. Can't reproduce it on OSX. I suspect it has something to do
    with ghostdriver/phantomjs run_script performance.

--- tests/wait_condition_tests.py
+++ tests/wait_condition_tests.py
@@ -10,36 +10,35 @@ class WaitConditionTests(unittest.TestCase):
         self.driver = get_driver()
         self.uri = make_temp_page("<div id='simple_id'>default text</div>")
 
-    def run_script(self, script, delay=0):
-        hiro.run_async(1, lambda: time.sleep(delay) or self.driver.execute_script(script))
+    def run_script(self, script, delay=0, sync=False):
+        return (hiro.run_sync if sync else hiro.run_async)(1, lambda: time.sleep(delay) or self.driver.execute_script(script))
 
     def build_page_object(self, condition):
         class P(Page):
             id_el = Element(Locators.ID, "simple_id"
                 , only_if=condition
-                , timeout=2)
+                , timeout=5)
         return P
 
     @hiro.Timeline(scale=10)
     def test_only_if_matches_text(self):
         page = self.build_page_object(MATCHES_TEXT("changed"))(self.driver, self.uri)
         self.assertTrue(page.id_el is None)
-        self.run_script('document.getElementById("simple_id").innerHTML="changed";', 1)
+        runner = self.run_script('document.getElementById("simple_id").innerHTML="changed";', 1)
         self.assertEqual(page.id_el.text, "changed")
 
 
     @hiro.Timeline(scale=10)
     def test_only_if_displayed(self):
         page = self.build_page_object(VISIBLE())(self.driver, self.uri)
-        self.run_script('document.getElementById("simple_id").style.display="none";')
-        self.assertTrue(page.id_el is None)
-        self.run_script('document.getElementById("simple_id").style.display="block";', 1)
+        runner = self.run_script('document.getElementById("simple_id").style.display="none";', sync=True)
+        runner = self.run_script('document.getElementById("simple_id").style.display="block";', 1)
         self.assertTrue(page.id_el is not None)
 
     @hiro.Timeline(scale=10)
     def test_only_if_invisible(self):
         page = self.build_page_object(INVISIBLE())(self.driver, self.uri)
         self.assertTrue(page.id_el is None)
-        self.run_script('document.getElementById("simple_id").style.display="none";', 1)
+        runner = self.run_script('document.getElementById("simple_id").style.display="none";', 1)
         self.assertTrue(page.id_el is not None)
 
