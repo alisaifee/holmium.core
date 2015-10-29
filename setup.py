6f217394fbb6f05c06b63820606ba03352cbece4
Ali-Akber Saifee <ali@indydevs.org>
2015-10-29 16:09:05 +0500

    Wrap setup function in main block

--- setup.py
+++ setup.py
@@ -10,31 +10,32 @@ __copyright__ = "Copyright 2014, Ali-Akber Saifee"
 from setuptools import setup, find_packages
 import os
 import sys
-this_dir = os.path.abspath(os.path.dirname(__file__))
-REQUIREMENTS = open(os.path.join(this_dir, 'requirements/main.txt'), 'rt').read()
+if __name__ == "__main__":
+    this_dir = os.path.abspath(os.path.dirname(__file__))
+    REQUIREMENTS = open(os.path.join(this_dir, 'requirements/main.txt'), 'rt').read()
 
-import versioneer
-versioneer.versionfile_source = "holmium/core/_version.py"
-versioneer.versionfile_build = "holmium/core/version.py"
-versioneer.tag_prefix = ""
-versioneer.parentdir_prefix = "holmium.core-"
+    import versioneer
+    versioneer.versionfile_source = "holmium/core/_version.py"
+    versioneer.versionfile_build = "holmium/core/version.py"
+    versioneer.tag_prefix = ""
+    versioneer.parentdir_prefix = "holmium.core-"
 
-setup(
-    name='holmium.core',
-    author = __author__,
-    author_email = __email__,
-    license = "MIT",
-    url="https://holmiumcore.readthedocs.org/en/latest/",
-    zip_safe = False,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    install_requires = REQUIREMENTS,
-    classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
-    description='selenium page objects and other utilities for test creation',
-    long_description=open('README.rst').read() + open('HISTORY.rst').read(),
-    packages = find_packages(exclude=["tests*"]),
-    entry_points = {
-        'nose.plugins.0.10': ['holmium = holmium.core:HolmiumNose',]
-        }
-)
+    setup(
+        name='holmium.core',
+        author = __author__,
+        author_email = __email__,
+        license = "MIT",
+        url="https://holmiumcore.readthedocs.org/en/latest/",
+        zip_safe = False,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        install_requires = REQUIREMENTS,
+        classifiers=[k for k in open('CLASSIFIERS').read().split('\n') if k],
+        description='selenium page objects and other utilities for test creation',
+        long_description=open('README.rst').read() + open('HISTORY.rst').read(),
+        packages = find_packages(exclude=["tests*"]),
+        entry_points = {
+            'nose.plugins.0.10': ['holmium = holmium.core:HolmiumNose',]
+            }
+    )
 
