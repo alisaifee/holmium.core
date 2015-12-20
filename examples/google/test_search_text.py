79ce708112987fc476664f32fda1ce38e13a1382
Ali-Akber Saifee <ali@indydevs.org>
2015-12-20 08:24:57 +0800

    Update examples

--- examples/google/test_search_text.py
+++ examples/google/test_search_text.py
@@ -2,16 +2,23 @@ import unittest
 import selenium.webdriver
 from holmium.core import Page, Element, Elements, Locators, ElementMap
 
+
 class GoogleMain(Page):
-    search_box = Element( Locators.NAME, "q", timeout = 1)
-    google_buttons = ElementMap ( Locators.CLASS_NAME, "gbts" , timeout = 1 )
-    search_results = Elements( Locators.CSS_SELECTOR, "li.g>div.rc", timeout = 1, value = lambda el : {
-        "link":el.find_element_by_css_selector("h3.r>a").get_attribute("href"),
-        "title":el.find_element_by_css_selector("h3.r>a").text
-        })
+    search_box = Element(Locators.NAME, "q", timeout=2)
+    google_buttons = ElementMap(
+        Locators.CSS_SELECTOR, ".jsb input", timeout=2,
+        key=lambda e: e.get_attribute("value")
+    )
+    search_results = Elements(
+        Locators.CSS_SELECTOR, "div.g>div.rc", timeout=2,
+        value = lambda el : {
+            "link": el.find_element_by_css_selector("h3.r>a").get_attribute("href"),
+            "title": el.find_element_by_css_selector("h3.r>a").text
+        }
+    )
 
     def search ( self, query ):
-        self.google_buttons["Search"].click() # self.google_buttons behaves just like a dictionary
+        self.google_buttons["Google Search"].click() # self.google_buttons behaves just like a dictionary
         self.search_box.clear() # self.search_box is now evaluated directly to a WebElement
         self.search_box.send_keys(query)
         self.search_box.submit()
@@ -27,8 +34,14 @@ class TextSearchTest(unittest.TestCase):
 
     def test_text_search_first_result(self):
         self.page.search("selenium") # execute the page object method search
-        self.assertEquals( self.page.search_results[0]["title"], "Selenium - Web Browser Automation")
-        self.assertEquals( self.page.search_results[0]["link"], "http://docs.seleniumhq.org/")
+        self.assertEquals(
+            self.page.search_results[0]["title"],
+            u"Selenium - Web Browser Automation"
+        )
+        self.assertEquals(
+            self.page.search_results[0]["link"],
+            u"http://www.seleniumhq.org/"
+        )
 
     def tearDown(self):
         self.driver.quit()
--- examples/wikipedia/test_search_article.py
+++ examples/wikipedia/test_search_article.py
@@ -1,20 +1,30 @@
 import unittest
+
 import selenium.webdriver
-from holmium.core import Page, Element, Elements, Locators, ElementMap
+
+from holmium.core import Page, Element, Locators, ElementMap
 
 
 class WikiPedia(Page):
-    languages = ElementMap( Locators.CLASS_NAME, "central-featured-lang"
-                                    , key = lambda el:el.get_attribute("lang")
-                                    , value = lambda el: el.find_element_by_tag_name("a"))
-    search_box = Element( Locators.CSS_SELECTOR, "input#searchInput" )
-    article_title = Element( Locators.CSS_SELECTOR, "h1#firstHeading span[dir=auto]" )
-    search_results = ElementMap( Locators.CSS_SELECTOR, "div.mw-search-result-heading>a")
-    def search(self,  query ):
+    languages = ElementMap(
+        Locators.CLASS_NAME, "central-featured-lang"
+        , key = lambda el:el.get_attribute("lang")
+        , value = lambda el: el.find_element_by_tag_name("a")
+    )
+    search_box = Element(
+        Locators.CSS_SELECTOR, "input#searchInput"
+    )
+    article_title = Element(
+        Locators.CSS_SELECTOR, "h1#firstHeading"
+    )
+    search_results = ElementMap(
+        Locators.CSS_SELECTOR, "div.mw-search-result-heading>a"
+    )
+
+    def search(self, query):
         self.search_box.clear()
         self.search_box.send_keys( query )
         self.search_box.submit()
-
 
 
 class TextSearchArticle(unittest.TestCase):
@@ -25,7 +35,10 @@ class TextSearchArticle(unittest.TestCase):
     def test_text_search_alllangs(self):
         for language in self.page.languages:
             self.page.go_home().languages[language].click()
-            self.assertEquals(self.page.search("google").article_title.text, "Google", language)
+            self.assertTrue(
+                self.page.search("google").article_title.text.startswith("Google"),
+                language
+            )
 
     def tearDown(self):
         self.driver.quit()
