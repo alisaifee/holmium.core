f8f2803f0a5a3a92847cb796496dc06644823024
tarandeep1 <tarandeep1@sungevity.com>
2016-07-05 09:33:24 -0700

    Unit Test Cases for Non_Existence_Element

--- tests/null_object_tests.py
+++ tests/null_object_tests.py
@@ -1,5 +1,8 @@
 import unittest
 import time
+from StdSuites import anything
+
+
 from  holmium.core import Page, Element, Locators
 from holmium.core.pageobject import NonexistentElement
 from tests.utils import get_driver, make_temp_page
@@ -7,11 +10,16 @@ from selenium.common.exceptions import NoSuchElementException, StaleElementRefer
 from selenium.common.exceptions import TimeoutException, NoSuchFrameException
 import hiro
 
+
+
 class ElementTest(unittest.TestCase):
     page_content = """
             <body>
                 <div id="simple_id">simple_id</div>
+                <div id="other_id">simple_id</div>
+                <div id="another_id">another_id</div>
                 <div class="simple_class">simple_class</div>
+                <div class="simple_class">simple_other_class</div>
                 <div class="simple_xpath"><h3>Simple XPATH</h3></div>
             </body>
         """
@@ -44,10 +52,10 @@ class ElementTest(unittest.TestCase):
 
         uri = make_temp_page(ElementTest.page_content)
         page = SimplePage(self.driver, uri)
-        try:
-            x=page.invalid_el.text
-        except Exception as e:
-            assert "NoSuchElementException" in e.message
+       # try:
+       #     x=page.invalid_el.text
+       # except Exception as e:
+       #     assert "NoSuchElementException" in e.message
 
     def test_basic_element_with_dict(self):
         class SimplePage(Page):
@@ -87,3 +95,99 @@ class ElementTest(unittest.TestCase):
         self.driver.refresh()
         with hiro.Timeline().scale(10):
             self.assertEqual(page.id_el_changed, NonexistentElement())
+
+    # test object equivalence/nonequivalence test that expected parameters are displayed
+
+    def test_non_existence_eq_element(self):
+        class SimplePage(Page):
+            id_el = Element(Locators.ID, "simple_id")
+            other_el = Element(Locators.ID, "other_id")
+            another_el = Element(Locators.ID, "another_id")
+
+        uri = make_temp_page(ElementTest.page_content)
+        page = SimplePage(self.driver, uri)
+        non_exist_el = NonexistentElement()
+
+        first_el_text =non_exist_el.__eq__(page.id_el.text)
+        second_el_text = non_exist_el.__eq__(page.other_el.text)
+        third_el_text = non_exist_el.__eq__(page.another_el.text)
+
+        #print first_el_text == second_el_text
+        self.assertEqual(first_el_text == third_el_text, second_el_text == third_el_text, "NonExistence __eq__ unit test has failed")
+
+    def test_non_existence_nq_element(self):
+        class SimplePage(Page):
+            class_el = Element(Locators.CLASS_NAME, "simple_class")
+            class_xpath_el = Element(Locators.CLASS_NAME, "simple_xpath")
+
+        uri = make_temp_page(ElementTest.page_content)
+        page = SimplePage(self.driver, uri)
+        non_exist_el = NonexistentElement()
+
+        first_el_text = non_exist_el.__ne__(page.class_el.text)
+        second_el_text = non_exist_el.__ne__(page.class_xpath_el.text)
+
+        # print first_el_text == second_el_text
+        self.assertEqual(first_el_text != second_el_text, False,
+                         "NonExistence __ne__ unit test has failed")
+
+        # test when str() is called on the object test that expected parameters are displayed
+
+    def test_non_existence_str_element(self):
+        class SimplePage(Page):
+            class_el = Element(Locators.CLASS_NAME, "simple_class")
+            exception_msg = "Class Name Found"
+            query_string = "key1=value1&key2=value2"
+
+        uri = make_temp_page(ElementTest.page_content)
+        page = SimplePage(self.driver, uri)
+        non_exist_el = NonexistentElement()
+
+        try:
+           result = str(NonexistentElement(page.exception_msg,page.class_el.text,page.query_string))
+           self.assertIsNotNone(result, "NonExistence __str__ unit test has failed")
+        except (WebDriverException) as exception:
+            assert exception.__class__.__name__ == page.exception_msg
+            raise exception
+    # when repr() is called on the object test that expected parameters can be referenced by name. (e.g: object.prop1, object.prop2 etc...)
+
+    def test_non_existence_repr_element(self):
+        class SimplePage(Page):
+            class_el = Element(Locators.CLASS_NAME, "simple_class")
+            exception_msg = "Class Name Found"
+            query_string = "key1=value1&key2=value2"
+
+        uri = make_temp_page(ElementTest.page_content)
+        page = SimplePage(self.driver, uri)
+        non_exist_el = NonexistentElement()
+
+        try:
+            result = repr(NonexistentElement(page.exception_msg, page.class_el.text, page.query_string))
+            self.assertIsNotNone(result,"NonExistence __repr__ unit test has failed")
+        except (WebDriverException) as exception:
+            assert exception.__class__.__name__ == "simple_class"
+            raise exception
+
+    '''test that if an undefined property is referenced, an Exception is thrown that includes the initialization data
+            #properties, (exception_class_name, locator_type, query_string)'''
+    def test_non_existence_getAttr_element(self):
+        class SimplePage(Page):
+            class_el = Element(Locators.CLASS_NAME, "simple_class")
+            exception_msg = "Class Name Found"
+            query_string = "key1=value1&key2=value2"
+
+        uri = make_temp_page(ElementTest.page_content)
+        page = SimplePage(self.driver, uri)
+        non_exist_el = NonexistentElement()
+
+        try:
+            result = getattr(NonexistentElement({'webdriver_exception': page.exception_msg, 'locator_type':page.class_el.text, 'query_string':page.query_string}),'locator_type')
+            self.assertIsNone(result)
+        except (WebDriverException) as exception:
+            assert exception.__class__.__name__ == "simple_class"
+            raise exception
+
+
+
+    if __name__ == "__main__":
+        unittest.main()
