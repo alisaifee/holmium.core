227c9559a37cf6105ff6e526056f867e0217d956
Ali-Akber Saifee <ali@indydevs.org>
2020-05-22 08:51:44 -0700

    Use rtd theme when building docs locally

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,6 +5,13 @@ import sys, os
 sys.path.insert(0, os.path.abspath('../../'))
 import holmium.core
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
 extensions = [
     'sphinx.ext.autodoc', 'sphinx.ext.autosummary',
     'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
