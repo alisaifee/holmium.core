108fe0c026bd35d8c13202234777a886dcc92157
Ali-Akber Saifee <ali@indydevs.org>
2023-02-28 10:43:17 -0800

    Update tagging script

--- tag.sh
+++ tag.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-dryrun=0
-while [ "$1" != "" ]; do
-    case $1 in
-        -d | --dryrun )    dryrun=1
-                           ;;
-    esac
-    shift
-done
-
-rm -rf build dist
 last_tag=$(git tag | sort -nr | head -n 1)
 echo current version:$(python setup.py --version), current tag: $last_tag
 read -p "new version:" new_version
-last_portion=$(grep -P "^History$" HISTORY.rst -5 | grep -P "^v\d+.\d+.\d+")
-changelog_file=/var/tmp/holmium.core.newchangelog
-new_changelog_heading=${new_version}
-new_changelog_heading_sep=$(python -c "print('='*len('$new_changelog_heading'))")
+last_portion=$(grep -P "^Changelog$" HISTORY.rst -5 | grep -P "^v\d+.\d+")
+changelog_file=/var/tmp/limiter.newchangelog
+new_changelog_heading="v${new_version}"
+new_changelog_heading_sep=$(python -c "print('-'*len('$new_changelog_heading'))")
 echo $new_changelog_heading > $changelog_file
 echo $new_changelog_heading_sep >> $changelog_file
 echo "Release Date: `date +"%Y-%m-%d"`" >> $changelog_file
@@ -24,15 +14,12 @@ cp HISTORY.rst.new HISTORY.rst
 vim -O HISTORY.rst <(echo \# vim:filetype=git;git log $last_tag..HEAD --format='* %s (%h)%n%b' | sed -E '/^\*/! s/(.*)/    \1/g')
 if rst2html.py HISTORY.rst > /dev/null
 then
-    echo "tagging $new_version"
+    echo "Tag $new_version"
     git add HISTORY.rst
     git commit -m "Update changelog for  ${new_version}"
-    git tag -s ${new_version} -m "Release version ${new_version}"
-    python setup.py build sdist bdist_egg bdist_wheel
-    test $dryrun != 1 && twine upload dist/*
+    git tag -s ${new_version} -m "Tag version ${new_version}"
     rm HISTORY.rst.new
 else
     echo changelog has errors. skipping tag.
 fi;
-
 
